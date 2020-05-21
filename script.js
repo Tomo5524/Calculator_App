bb4380b367703e96e78548b36dd51f85dc6a4e9e
Tomo5524 <ttoommoosrst@gmail.com>
2020-05-21 21:48:22 +0900

    completed

--- script.js
+++ script.js
@@ -54,16 +54,12 @@ function operate(n1,n2,o){
 }
 
 
-// delete functon
-
-input.addEventListener('input',(e)=>{
+input.value = '0';
+input.focus()
+input.addEventListener('input',(e)=>{  
     main(e);
 });
 
-// clear.addEventListener('click',main);
-
-// // put these in main function
-// del.addEventListener('click',main);
 const btn = document.querySelectorAll('button')
 btn.forEach(val =>{
     val.addEventListener('click',(e) =>{
@@ -73,40 +69,44 @@ btn.forEach(val =>{
 
 let cur_num = '0';
 let storeVal = '0';
-input.value = '0';
+let delete_input = false;
 
 
 // input value and string value are the same
 function main(e){
-    
-    console.log(input.value) // prints values in display
-    
+    console.log("NEW INPUT")
     const opeArr = ['+','-','*','/']
-    let CantHaveZeroNextToOpe = false;
-    let userInput = ''
-    let res = ''
-    let delete_input = false;
-
-    if(e.type = 'input'){
-        userInput = e.data;
+    const allValues = ['+','-','*','/','.','1','2','3','4','5','6','7','8','9','0']
+    let userInput = '';
+    let res = '';
+    // console.log(e)
+    // when user type input
+    if(e.type === 'input'){
+        if (allValues.includes(e.data)){
+            userInput = e.data;
+        }
     }
 
-    if(e.type = 'click'){
+    // when user click button
+    if(e.type === 'click'){
         userInput = e.target.innerHTML;
     }
 
-    storeVal += userInput
-    console.log(storeVal)
-    
+    console.log(userInput,'userinput')
+    console.log(input.value,'input.valueB4Processed')
+
+    // console.log(delete_input,'delete_input')
+
     if (delete_input){
-        input.value = '';
+        input.value = '0';
         delete_input = false;
-        console.log('a')
+        console.log('RestInput')
     }
 
     if (userInput === "clear"){
-        input.value = '0'
-        cur_num = '0'
+        input.value = '0';
+        cur_num = '0';
+        storeVal = '0';
         console.log('b')
     }
 
@@ -127,52 +127,130 @@ function main(e){
             console.log('d')
         }
         input.value = input.value.slice(0,input.value.length-1)
+        storeVal = storeVal.slice(0,input.value.length-1) 
+        console.log('w')
     }
 
     // if input is not a number
     else if(isNaN(userInput)){
-        
         if (isNaN(input.value.slice(-1))){ // if last input is not number
             if (userInput !== '.' && userInput !== '='){ // if input is strictly operator
-                input.value = input.value.slice(0,input.value.length-1) + userInput // deal with operator swap
+                if(opeArr.includes(storeVal.slice(-1))){
+                    storeVal = storeVal.slice(0,storeVal.length-1) + userInput; // swap ope
+                    input.value = storeVal;
+                    console.log('e')
+                }
+                // accept ope
+                else{
+                    console.log('acceptOpe')
+                    storeVal += userInput // if cur_num dosenst have dot, accept it
+                    input.value = storeVal
+                    // input.value += userInput 
+                    
+                }
                 cur_num = '0'; // update cur_num 
-                console.log('e')
+                
+            }
+            else if(userInput === '.'){
+                if (input.value.slice(-1) !== '.' && input.value.indexOf('.') === -1){
+                    console.log('acceptDot')
+                    storeVal += userInput // if cur_num dosenst have dot, accept it
+                    input.value = storeVal
+                    // input.value += userInput 
+                    cur_num += userInput
+                }
+                // 
+                else{
+                    if (cur_num==='0'){
+                        console.log('acceptDot')
+                        storeVal += userInput // if cur_num dosenst have dot, accept it
+                        input.value = storeVal
+                        // input.value += userInput 
+                        cur_num += userInput
+                    }
+
+                    else{
+                        // avoide duplicate dots
+                        if (cur_num.indexOf('.') !== -1){
+                            console.log('acceptDotNoDup')
+                            storeVal = storeVal.slice(0,storeVal.length-1) + userInput; 
+                            input.value = storeVal;
+                            cur_num = cur_num.slice(0,cur_num.length-1) + userInput
+                        }
+
+                    }
+                }
+                
+                
             }
             
         }
-        else if(userInput === '.'){ // if last input is number
+        else if(userInput === '.'){ // if input is .
+            console.log(cur_num,'inputisDOT')
             if (cur_num.indexOf('.') === -1){
-                input.value += userInput // if cur_num dosenst have dot, accept it
+                storeVal += userInput // if cur_num dosenst have dot, accept it
+                input.value = storeVal
+                // input.value += userInput 
                 cur_num += userInput
                 console.log('f')
             }
-            console.log('g')
+            console.log('g') 
+            // console.log(cur_num)
         }
-        else{ // if userinput is '='
-            
-            res = processData(input.value).toString() // get answer
-            let processedRes = parseFloat(res).toFixed(10)
-            if (res.indexOf('.') !== -1 && res !== "Error" && res !== "0" && 1 < res.slice(res.indexOf('.')+1).length){
-                // round
-                let dotIdx = res.indexOf('.')
-                processedRes = processedRes.toString()
-                // if number after dot is different, round up
-                if (processedRes[dotIdx+1] !== res[dotIdx+1]){
-                    input.value = parseFloat(res).toFixed(1)
+        else{ // if userinput is '=' or operator
+            if (opeArr.includes(userInput)){
+                if (opeArr.includes(cur_num.slice(-1))){
+                    input.value = input.value.slice(0,input.value.length-1) + userInput // if cur_num is only one length, swap 0 with userinput
+                    cur_num = cur_num.slice(0,cur_num-1) + userInput // if cur_num is only one length, swap 0 with userinput
+                    console.log('ope')
                 }
-                // no round 
                 else{
-                    input.value = parseFloat(res).toFixed(10);
+                    storeVal += userInput
+                    input.value = storeVal
+                    cur_num = '' // reset curnum
+                    console.log('accept ope')
                 }
-            
-            // if no float and calculatable number    
-            }else{
-                input.value = res
             }
-            //input.value = res.indexOf('.') === -1 ? res : parseFloat(res).toFixed(10);
-            // reset all accumulated values 
-            cur_num = '0';
-            delete_input = true; // delete result
+            else{
+                res = processData(input.value).toString()
+                // input.value = res
+                // handle decimal
+                // if rounded, cut off alll decimals 
+                console.log(res,'meow',res.indexOf('.'),typeof res)
+                console.log(parseFloat(res),'float')
+                let processedRes = parseFloat(res).toFixed(10)
+                console.log(res.slice(res.indexOf('.')).length,'length')
+                if (res.indexOf('.') !== -1 && res !== "Error" && res !== "0" && 1 < res.slice(res.indexOf('.')+1).length){
+                    // round
+                    let dotIdx = res.indexOf('.')
+                    console.log(dotIdx,'dotIdx')
+                    processedRes = processedRes.toString()
+                    
+                    // if number after dot is different, round up
+                    if (processedRes[dotIdx+1] !== res[dotIdx+1]){
+                        input.value = parseFloat(res).toFixed(1)
+                    }
+                    // no round 
+                    else{
+                        // 0.02+0.230 error
+                        // 12+7-5*41/21 okay
+                        input.value = parseFloat(res).toFixed(10);
+                        
+                    }
+                
+                // if no float and calculatable number    
+                }else{
+                    input.value = res
+                }
+                //input.value = res.indexOf('.') === -1 ? res : parseFloat(res).toFixed(10);
+                // reset all accumulated values 
+                cur_num = '0';
+                storeVal = '0';
+                delete_input = true; // delete result
+                console.log('calculated')
+                console.log(delete_input,'delete_input')
+
+            }
         }
         
         
@@ -183,31 +261,114 @@ function main(e){
         // when input is 0, deals with duplicates 
         if(userInput === '0'){
             if (cur_num.length !== 1){ 
-            input.value += userInput
-            cur_num += userInput
-            console.log('i')
+                // avoid 0 duplicates
+                if (cur_num.slice(-1) === '0'){
+                    storeVal = storeVal.slice(0,storeVal.length-1) + userInput; 
+                    input.value = storeVal;
+                    cur_num = cur_num.slice(0,cur_num.length-1) + userInput
+                    console.log('0Duplicates')
+                }
+                else{
+                    storeVal += userInput;
+                    input.value = storeVal;
+                    cur_num += userInput
+                    console.log('accceptZero')
+                }
+                
+                
             }
             else{ // if cur_num's length is 1 and last char is not 0, accept it
-                if (cur_num.slice(-1) !== '0'){
-                    input.value += userInput
-                    cur_num += userInput
+                if (cur_num.length === 1 && cur_num.slice(-1) !== '0'){
+                    storeVal += userInput;
+                    input.value = storeVal;
+                    cur_num += userInput;
+                    console.log('263LN')
                 } 
-                console.log('j')
+                else{
+                    // 0.02+0
+                    if(opeArr.includes(storeVal.slice(-1))){
+                        storeVal += userInput;
+                        input.value = storeVal;
+                        cur_num += userInput;
+                    }
+                    else{
+                        storeVal = storeVal.slice(0,storeVal.length-1) + userInput; 
+                        input.value = storeVal;
+                        cur_num = cur_num.slice(0,cur_num.length-1) + userInput
+                        console.log('269LN')
+                    }
+                    
+                }
+              
             }
         }
         // if userinput is not 0
-        else if(cur_num.length === 1 ){
-            input.value = input.value.slice(0,input.value.length-1) + userInput // if cur_num is only one length, swap 0 with userinput
-            console.log('k')
-        }
         else{
-            input.value += userInput
-            cur_num += userInput 
-            console.log('l')
+            // if cur_num is length one and is 0, swap
+            // if last char of value is ope, dont execute
+            console.log(cur_num,'cur_numB4processed')
+            console.log(storeVal,'storeValB4processed')
+            if(cur_num.length <= 1 && input.value.slice(-1) === '0'){
+                // if storeval's last char is ope, update displayinput and swap cur_num with userinput
+                if (opeArr.includes(input.value.slice(-1))){
+                    cur_num = userInput;
+                    storeVal += userInput
+                    input.value = storeVal;
+                    
+                    console.log('lastinputwasope')
+                }
+                // if storeval's last char is not ope, needs swap
+                else{
+                    
+                    storeVal = storeVal.slice(0,storeVal.length-1) + userInput; // if cur_num is only one length, swap 0 with userinput
+                    input.value = storeVal;
+                    cur_num = cur_num.slice(0,cur_num.length-1) + userInput // if cur_num is only one length, swap 0 with userinput
+                    console.log('K Swap')
+                }
+            }
+            else{
+                if(cur_num === '0'){
+                    if(opeArr.includes(storeVal.slice(-1))){
+                        storeVal += userInput
+                        input.value = storeVal
+                        cur_num += userInput
+                        console.log('acceptCuzPreviousinputwasOpe')
+                    }
+                    else{
+                        // when typed input is not on button, e.g letter
+                        if (!userInput){
+                            storeVal += userInput
+                            input.value = storeVal
+                        }
+                        else{
+                            storeVal = storeVal.slice(0,storeVal.length-1) + userInput; // if cur_num is only one length, swap 0 with userinput
+                            input.value = storeVal; 
+                            cur_num = userInput
+                            console.log('swap cuz curnum is 0')
+                        }
+                        
+                    }
+                    
+                }else{
+                    storeVal += userInput
+                    input.value = storeVal
+                    cur_num += userInput
+                    console.log('curNumLength greter than 1')
+                }
+                
+            }
+            
         }
         
        
     }
+    // delete this and put storeval where needed
+    // storeVal += userInput
+    console.log(input.value,'inputvalue') // prints values in display
+    console.log(storeVal,'storeval')
+    console.log(cur_num,'curnum')
+    input.focus() 
+
 }       
 
 
